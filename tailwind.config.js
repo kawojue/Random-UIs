e20a5477bbbd9f606ca5a80d2079c7f36fb34062
Raheem Kawojue <kawojue08@gmail.com>
2023-06-16 14:47:09 +0100

    [add] colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        "pry-clr-0": 'rgb(75, 20, 91)',
+        "pry-clr-1": 'rgb(16, 42, 66)',
+        "pry-clr-2": 'rgb(180, 52, 92)',
+        "pry-clr-3": 'rgb(234, 230, 235)',
+        "pry-clr-4": 'rgb(50, 77, 103)'
+      },
     },
   },
   plugins: [],
